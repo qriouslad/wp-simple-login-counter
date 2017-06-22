9ee66936b0ae337fc8213195e6bf44b408c591af
CodeGood <codegood.io@gmail.com>
2017-06-22 10:25:24 +0000

    Implement class and functions to count and display user login.

--- wp-simple-login-counter.php
+++ wp-simple-login-counter.php
@@ -23,3 +23,96 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with {Plugin Name}. If not, see {License URI}.
 */
+
+namespace Qriouslad\WordPressPlugin;
+
+class Login_Counter {
+
+	public function init() {
+
+			add_action( 'wp_login', array( $this, 'count_user_login' ), 10, 2 );
+
+			add_filter( 'manage_users_columns', array( $this, 'add_stats_column' ) );
+
+			add_action( 'manage_users_custom_column', array( $this, 'fill_stats_column' ), 10, 3 );
+	}
+
+	/**
+	 * Save user login count to database
+	 *
+	 * @param string $user_login username
+	 * @param object $user WP_User object
+	 */
+	public function count_user_login( $user_login, $user ) {
+
+		if ( !empty( get_user_meta( $user->ID, 'sp_login_count', true) ) ) {
+
+			$login_count = get_user_meta( $user->ID, 'sp_login_count', true );
+			update_user_meta( $user->ID, 'sp_login_count', ( (int) $login_count + 1 ) );
+
+		} else {
+
+			update_user_meta( $user->ID, 'sp_login_count', 1 );
+
+		}
+
+	}
+
+
+	/**
+	 * Add login stats column to the users listing page
+	 *
+	 * @param string $columns
+	 *
+	 * @return mixed
+	 */
+	public function add_stats_column( $columns ) {
+		$columns['login_stat'] = __( 'Login Count' );
+		return $columns;
+	}
+
+
+	/**
+	 * Fill the stat column with values
+	 *
+	 * @param string $empty
+	 * @param string $column_name
+	 * @param int $user_id
+	 *
+	 * @return string|void
+	 */
+	public function fill_stats_column( $empty, $column_name, $user_id ) {
+
+		if ( 'login_stat' == $column_name ) {
+
+			if ( get_user_meta( $user_id, 'sp_login_count', true ) !== '' ) {
+					$login_count = get_user_meta( $user_id, 'sp_login_count', true );
+					return '<strong>' . $login_count . '</strong>';
+			} else {
+				return __( 'No record found.' );
+			}
+
+		}
+
+		return $empty;
+
+	}
+
+
+	/**
+	 * Singleton class instance
+	 * @return Login_Counter
+	 */
+	public static function get_instance() {
+		static $instance;
+		if ( ! isset( $instance ) ) {
+			$instance = new self();
+			$instance->init();
+		}
+
+		return $instance;
+	}
+
+}
+
+Login_Counter::get_instance();
