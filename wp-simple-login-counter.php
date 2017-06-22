dc705cca87ad642d1735d76a2a539d1358d63c52
CodeGood <codegood.io@gmail.com>
2017-06-22 10:02:22 +0000

    Initial commit: plugin header.

---
+++ wp-simple-login-counter.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Simple Login Counter
+Plugin URI:  https://github.com/qriouslad/wp-simple-login-counter
+Description: Simple user login counter plugin for WordPress
+Version:     1.0
+Author:      Bowo
+Author URI:  https://bowo.io
+Text Domain: wpscf
+Domain Path: /languages
+License:     GPL2
+ 
+WP Simple Login Counter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP Simple Login Counter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.
+*/
